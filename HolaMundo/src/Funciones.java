53e8ded4f4c678bd8cbd55bf6f736d79f4002d28
Ariel <arielantequiera@gmail.com>
2022-11-14 21:54:45 -0300

    Funciones

---
+++ HolaMundo/src/Funciones.java
@@ -0,0 +1,29 @@
+public class Funciones {
+    public static void main(String[] args) {
+        double y = 3;
+
+        // Area de un circulo
+        // pi * r2
+        circleArea(y);
+
+        // Area de una esfera
+        // 4*PI*r2
+        sphereArea(y);
+
+        // Volumen de una esfera
+        // (4/3)*pi * r3
+        sphereVolumen(y);
+    }
+
+    public static double circleArea(double r) {
+        return Math.PI * Math.pow(r, 2);
+    };
+
+    public static double sphereArea(double r) {
+        return 4 * Math.PI * Math.pow(r, 2);
+    };
+
+    public static double sphereVolumen(double r) {
+        return (4 / 3) * Math.PI * Math.pow(r, 3);
+    }; 
+}
